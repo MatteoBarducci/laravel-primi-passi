faf139c65d273cf94ae9dd6f4d41fe678a76b06f
MatteoBarducci <mbardu1301@gmail.com>
2024-05-19 22:24:22 +0200

    add data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $data = [
+        'nome' => 'Mario',
+        'cognome' => 'Rossi'
+    ];
+
+    return view('homepage', $data);
 });
